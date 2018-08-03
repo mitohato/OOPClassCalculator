3a670fd76308bd38a20b233696e1060b80696be9
mitohato <mitohato14@gmail.com>
2018-08-03 13:24:50 +0900

    Add OPButtons class

---
+++ OPButtons.java
@@ -0,0 +1,49 @@
+/**
+ * OPButtons
+ */
+public class OPButtons {
+  private Button plusButton;
+  private Button subButton;
+  private Button timeButton;
+  private Button devideButton;
+  private Button equalButton;
+  private Button modButton;
+  private Button changeSignButton;
+  private Button acButton;
+
+  OPButtons() {
+    plusButton = new Button();
+    subButton = new Button();
+    timeButton = new Button();
+    devideButton = new Button();
+    equalButton = new Button();
+    modButton = new Button();
+    changeSignButton = new Button();
+    acButton = new Button();
+  }
+
+  Button getPlusButton() {
+    return plusButton;
+  }
+  Button getSubButton() {
+    return subButton;
+  }
+  Button getTimeButton() {
+    return timeButton;
+  }
+  Button getDevideButton() {
+    return devideButton;
+  }
+  Button getEqualButton() {
+    return equalButton;
+  }
+  Button getModButton() {
+    return modButton;
+  }
+  Button getChangeSignButton() {
+    return changeSignButton;
+  }
+  Button getACButton() {
+    return acButton;
+  }
+}

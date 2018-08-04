e0278620190d2c679e272b6c6d133e344b2825e9
mitohato <mitohato14@gmail.com>
2018-08-05 05:41:45 +0900

    Change squares for GridLayout

--- CalculatorFrame.java
+++ CalculatorFrame.java
@@ -30,7 +30,7 @@ class CalculatorFrame extends Frame implements ActionListener {
         );
 
         setLayout(new GridLayout(
-                4,
+                2,
                 1
         ));
 
